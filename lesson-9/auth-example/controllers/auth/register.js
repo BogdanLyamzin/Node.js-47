e74315cd8a4f078e2104163d450710bddd673222
unknown <bogdan.lyamzin.d@gmail.com>
2022-10-29 12:03:32 +0300

    finish lesson 9

--- lesson-9/auth-example/controllers/auth/register.js
+++ lesson-9/auth-example/controllers/auth/register.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs")
+const gravatar = require("gravatar")
 
 const {User} = require("../../models/user")
 
@@ -10,8 +11,9 @@ const register = async(req, res)=> {
     if(user) {
         throw RequestError(409, "Email in use")
     }
-    const hashPassword = await bcrypt.hash(password, 10)
-    const result = await User.create({name, email, password: hashPassword});
+    const hashPassword = await bcrypt.hash(password, 10);
+    const avatarURL = gravatar.url(email);
+    const result = await User.create({name, email, password: hashPassword, avatarURL});
 
     res.status(201).json({
         name: result.name,
--- lesson-9/auth-example/routes/api/auth.js
+++ lesson-9/auth-example/routes/api/auth.js
@@ -4,7 +4,7 @@ const ctrl = require("../../controllers/auth")
 
 const { ctrlWrapper } = require("../../helpers");
 
-const {validateBody, authenticate} = require("../../middlewares")
+const {validateBody, authenticate, upload} = require("../../middlewares")
 
 const {schemas} = require("../../models/user")
 
@@ -19,5 +19,7 @@ router.post("/login", validateBody(schemas.loginSchema), ctrlWrapper(ctrl.login)
 router.get("/current", authenticate, ctrlWrapper(ctrl.getCurrent))
 
 router.get("/logout", authenticate, ctrlWrapper(ctrl.logout))
+
+router.patch("/avatars", authenticate, upload.single("avatar"), ctrlWrapper(ctrl.updateAvatar))
 
 module.exports = router;
